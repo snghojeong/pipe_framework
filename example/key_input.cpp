5094663309083eedd1e818400074948d6edaa308
Seungho Jeong <snghojeong@gmail.com>
2022-06-19 22:43:13 +0900

    Update key_input.cpp

--- example/key_input.cpp
+++ example/key_input.cpp
@@ -2,17 +2,17 @@
 int main(int argc, char** argv)
 {
     auto engine = make_unique<pipef::engine>(pipef::engine::create());
-    auto src = engine.create<key_input_src>();
-    auto filter = engine.create<character_filter>();
-    auto sink = engine.create<print_sink>();
+    auto src = engine->create<key_input_src>();
+    auto filter = engine->create<character_filter>();
+    auto sink = engine->create<print_sink>();
     
     filter->set_blist("abcd");
 
     src | filter | sink;
 
-    engine.run(-1 /* loop count */, 10000 /* duraion ms */);
+    engine->run(-1 /* loop count */, 10000 /* duraion ms */);
     
-    cout << "End of program." << src.get() << sink.get();
+    cout << "End of program." << src->get() << sink->get();
 
     return 0;
 }
