09b8d3c7b7ea4db02ad0436bb32c57bf13f04476
Seungho Jeong <snghojeong@gmail.com>
2022-07-07 15:03:56 +0900

    Update key_input.cpp

--- example/key_input.cpp
+++ example/key_input.cpp
@@ -7,6 +7,7 @@ int main(int argc, char** argv)
     auto sink = engine->create<print_sink>();
 
     src | filter["abcd"] | sink[stdout];
+          filter["asdf"] | sink[stderr];
 
     engine->run(-1 /* loop count */, 10000 /* duraion ms */);
     
