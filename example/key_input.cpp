14e1267d2acbda1e3ed3a3450a30ad22e03c525d
Seungho Jeong <snghojeong@gmail.com>
2022-08-19 23:53:12 +0900

    Update key_input.cpp

--- example/key_input.cpp
+++ example/key_input.cpp
@@ -10,6 +10,7 @@ int main(int argc, char** argv)
     src | filter["*"] | sink[stdout];
           filter["&"] | sink[stderr];
     src | map[[](data_uptr dat){ std::cout << dat->get() << std::endl; }] | sink[stdout];
+    src | filter["help"] | map[[](data_uptr dat){ return std::string("Help string..."); }] | sink[stdout];
 
     engine->run(INFINITE /* loop count */, 10000 /* duraion ms */);
     
