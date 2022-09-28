318ea90750437d77ee2db0e5b96693347f6a6764
Seungho Jeong <snghojeong@gmail.com>
2022-09-28 20:54:53 +0900

    Update key_input.cpp

--- example/key_input.cpp
+++ example/key_input.cpp
@@ -3,17 +3,14 @@ int main(int argc, char** argv)
 {
     auto engine = make_unique<pipef::engine>(pipef::engine::create());
     auto src = engine->create<key_input_src>();
-    auto filter = engine->create<character_filter>();
     auto help_filter = engine->create<character_filter>();
     auto sink = engine->create<print_sink>();
     auto src_cnter = engine->create<input_counter>();
-    auto fltr_cnter = engine->create<input_counter>();
     auto help_fltr_cnter = engine->create<input_counter>();
 
-    src | filter["*"] | sink[stdout];
+    src | sink[stdout];
     src | help_filter["help"] | map[[](data_uptr d){ return std::string("Help string..."); }] | sink[stdout];
     src | src_cnter;
-    filter | fltr_cnter;
     help_filter | help_fltr_cnter;
 
     engine->run(INFINITE /* loop count */, INFINITE /* duraion ms */);
