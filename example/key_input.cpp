107dfa5f20a35b26a9600e37c6876986946b5940
Seungho Jeong <snghojeong@gmail.com>
2022-11-01 20:31:12 +0900

    Update key_input.cpp

--- example/key_input.cpp
+++ example/key_input.cpp
@@ -6,18 +6,15 @@ int main(int argc, char** argv)
     auto help_filter = engine->create<character_filter>();
     auto command_mapper = engine->create<command_map>();
     auto sink = engine->create<print_sink>();
-    auto src_cnter = engine->create<input_counter>();
 
     src | sink[stdout];
     src | help_filter["help"] | map[[](data_uptr d){ return std::string("Help string... %s", d->to_string()); }] | sink[stdout];
     src | command_mapper["history"].set(hist_cmd_func);
     src | command_mapper["quit"].set(exit);
-    src | src_cnter;
 
     engine->run(INFINITE /* loop count */, INFINITE /* duraion ms */);
     
     std::cout << "End of program." << std:endl;
-    std::cout << "Count of key:" << src_cnter->get() << std:endl;
 
     return 0;
 }
